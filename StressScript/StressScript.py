dc4cf3c55fd63d280a911b3b600c6792057c5ec9
高子雄 <cuber_messenger@hotmail.com>
2019-12-12 23:59:32 +0800

    c

--- StressScript/StressScript.py
+++ StressScript/StressScript.py
@@ -286,13 +286,13 @@ def test():
 
 
 def test_1000():
-    
+    InitCostList = []
+    RequestCostList = []
     sales_num = 1
     customer_num = 1
     coupon_num = 1
     
     for t in range(0, 10):
-    
         sales_list = []
         customer_list = []
         coupon_list = []
@@ -314,26 +314,23 @@ def test_1000():
                 coupon_num += 1
                 
         session = requests.session()
-        
         start = time.time()
         
         for i in range(0, 10):
-            
             _ = TestRegistration(sales_list[i], session)
-            _ = TestRegistration(customer_list[i], session)
-            
             _ = TestLogin(sales_list[i], session)
-            _ = TestLogin(customer_list[i], session)
-            
-        end = time.time()
-        print("Register and Login cost: %.3f" % (end - start))
-        
-        start = time.time()
-        
+            _ = TestRegistration(customer_list[i], session)
+
         for i in range(0, 10):
             for j in range(0, 10):
                 _ = TestAddCoupons(sales_list[i], coupon_list[i*10+j], session)
-        
+        end = time.time()
+        InitCostList.append(end - start)
+        start = time.time()
+
+        for i in range(0, 10):
+            _ = TestLogin(customer_list[i], session)
+            
         for i in range(0, 10):
             for j in range(0, 10):
                 _ = TestCustomerGetCoupons(customer_list[i], sales_list[j], coupon_list[j*10+i], session)
@@ -349,9 +346,11 @@ def test_1000():
                 _ = TestGetCouponsInfo(searcher=customer_list[i], searchee=sales_list[j], session=session)
         
         end = time.time()
-        print("Other test cost: %.3f" % (end - start))
+        RequestCostList.append(end - start)
         
         session.close()
+    for i in range(10):
+        print("Request Cost: %.3f\tInit Cost: %.3f" % (RequestCostList[i], InitCostList[i]))
 
 if __name__ == '__main__':
     #test()
